794fd5f725e029a167d3b4fffd084d0cff79ea2a
Mathieu-R <mathieu0709@gmail.com>
2017-11-16 08:24:17 +0100

    add type to consumption endpoint and calculate first and last value on the server side. Not dry for now.

--- controllers/consumption.js
+++ controllers/consumption.js
@@ -16,11 +16,61 @@ async function add(req, res) {
 
 async function get(req, res) {
   if (req.query.start && req.query.end) {
-    const {start, end} = req.query;
-    res.status(200).json(await Consumption.find({serial: req.user.serial, date: {$gte: start, $lte: end}}));
+    const {start, end, type} = req.query;
+    const consumption = await Consumption.find({serial: req.user.serial, date: {$gte: start, $lte: end}});
+    res.status(200).json(getConsumptionAccordingToType(consumption, type));
     return;
   }
   res.status(200).json(await Consumption.find({serial: req.user.serial}));
+}
+
+function getConsumptionAccordingToType (consumption, type) {
+  switch (type) {
+    case 'year':
+      return consumption.reduce((prev, current) => {
+        const month = new Date(current).getMonth();
+        if (prev[month] && prev[month].start) {
+          prev[month].end = current.value;
+        } else {
+          prev[month].start = current.value;
+        }
+        return prev;
+      }, {});
+      break;
+    case 'month':
+      return consumption.reduce((prev, current) => {
+        const day = new Date(current).getDate();
+        if (prev[day] && prev[day].start) {
+          prev[day].end = current.value;
+        } else {
+          prev[month].start = current.value;
+        }
+        return prev;
+      }, {});
+      break;
+    case 'week':
+      return consumption.reduce((prev, current) => {
+        const day = new Date(current).getDay();
+        if (prev[day] && prev[day].start) {
+          prev[day].end = current.value;
+        } else {
+          prev[day].start = current.value;
+        }
+        return prev;
+      }, {});
+      break;
+    case 'day':
+      return consumption.reduce((prev, current) => {
+        const hour = new Date(current).getHours();
+        if (prev[hour] && prev[hour].start) {
+          prev[hour].end = current.value;
+        } else {
+          prev[hour].start = current.value;
+        }
+        return prev;
+      }, {});
+      break;
+  }
 }
 
 module.exports = {
