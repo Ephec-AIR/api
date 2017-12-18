70cc2d5af8828570081db591bfb5547f4ac4f5d7
Mathieu Rousseau <mathieu0709@gmail.com>
2017-12-18 13:30:09 +0100

    remove jwt in cookies 'cause we don't put jwt in cookies.

--- middlewares/authorizations.js
+++ middlewares/authorizations.js
@@ -7,8 +7,6 @@ const parseJWT = jwt({
   getToken: function (req) {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
       return req.headers.authorization.split(' ')[1];
-    } else if (req.cookies && req.cookies.jwt) {
-      return req.cookies.jwt;
     }
     return null;
   }
